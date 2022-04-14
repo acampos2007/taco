1b94fd1cb841e4059cfeafc81fb446b80888f702
Angel Campos <campos.angel@student.greenriver.edu>
2022-04-14 12:38:22 -0400

    added config file composer.json,and controller page added index.php, and views folder

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+
+//Turn on error reporting
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
+//Require the autoload file
+require_once('vendor/autoload.php');
+
+//Create an instance of the Base Class
+$f3 = Base::instance();
+//echo gettype($f3);
+
+//Define a default route
+$f3->route('GET /', function()
+{
+    $view = new Template();
+    echo $view->render('views/home.html');
+
+    //echo '<h1>Hello, World!</h1>';
+}
+);
+
+//Run fat free
+$f3->run();
